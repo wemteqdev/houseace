c5359c75c1dd0cca57c37930787a8b0c216f1028
alex <alexander.vasiliev87@yandex.com>
2018-10-25 09:56:18 +1000

    scrapper update

--- scrapper.php
+++ scrapper.php
@@ -66,6 +66,52 @@ class domain_api
     }
 }
 
+$houseace_base_url = "https://www.houseace.com.au";
+$houseace_authorization_user = 'Developer';
+$houseace_authorization_token = 'u17b I3Od Y7ZG d5Wq GRtT EUys';
+
+// $houseace_base_url = "https://houseace.wemteq.com";
+// $houseace_authorization_token = 'D6Fz 8VCt Q5Nv BMxe MVf0 nOX8';
+// $houseace_authorization_user = 'admin';
+
+// $houseace_base_url = "http://houseace.loc:8888";
+// $houseace_authorization_token = 'q929 Cs8K l49A boxt rTue Mc9t';
+// $houseace_authorization_user = 'admin';
+
+$wp_headers = array (
+    'Authorization' => 'Basic ' . base64_encode( $houseace_authorization_user . ':' . $houseace_authorization_token  ),
+);
+
+function get_houseace_listing_post($listing_id)
+{
+    global $houseace_base_url, $wp_headers;
+
+    $filters = array(
+     'filter[meta_key]'=>'id',
+     'filter[meta_value]'=> $listing_id
+    );
+
+    $filter_string = http_build_query($filters);
+    $url = $houseace_base_url .  '/wp-json/wp/v2/listing?' . $filter_string;
+
+    print("searching houseace listing with id:" . $listing_id  . " \n");
+
+    $response = wp_remote_get( $url, array (
+     'headers' => $wp_headers,
+    ));
+
+    $wp_listings = json_decode($response['body']);
+    $wp_listing = null;
+
+    if(count($wp_listings) > 0)
+    {
+        $wp_listing = $wp_listings[0];
+    }else{
+        print("Not Found: " . $url  . " \n");
+    }
+
+    return $wp_listing;
+}
 
 // prepare sales api client to pull sales results from domain.com.au
 $sales_client_id = 'client_538dd6d34c31418795c03e850d58b81c';
@@ -80,13 +126,13 @@ print("fetching sales results head \n");
 $results_header_data = $sales_api->getRequest('https://api.domain.com.au/v1/salesResults/_head');
 $results_header_json = json_decode($results_header_data);
 print_r($results_header_json);
-sleep(2);
+sleep(3);
 
 // get sydney sales listings
 print("fetching sydney sales listings \n");
 $results_data = $sales_api->getRequest('https://api.domain.com.au/v1/salesResults/Sydney/listings');
 $listings_json = json_decode($results_data);
-sleep(2);
+sleep(3);
 print("listings: ". count($listings_json) . "\n");
 
 // prepare listing api client
@@ -95,55 +141,26 @@ $listings_client_secrets = 'secret_7e2b4ce9a8608ad91394e9b89e32758f';
 
 $listings_api = new domain_api($listings_client_id, $listings_client_secrets);
 $listings_api->authorize();
-sleep(2);
+sleep(3);
 
 
-//$wp_headers = array (
-//     'Authorization' => 'Basic ' . base64_encode( 'admin' . ':' . '1vdR 4lVM ccLX 00zn suaK kVNr'  ),
-//);
-
-$wp_headers = array (
-     'Authorization' => 'Basic ' . base64_encode( 'Developer' . ':' . 'Ydmh Ez7o j1A2 fDxZ jHTc g4B3'  ),
-);
 $inserted_count = 0;
+
 foreach($listings_json as $listing)
 {
-
     // check if listing already exists in houseace wordpress
-    print("searching houseace listing with id:" . $listing->id  . " \n");
-    $filters = array(
-     'filter[meta_key]'=>'id',
-     'filter[meta_value]'=> $listing->id
-    );
+    $wp_listing = get_houseace_listing_post($listing->id);
 
-    $filter_string = http_build_query($filters);
-
-    //$url = rest_url( 'wp/v2/listings?' . $filter_string  );
-    $url = "https://houseace.com.au/wp-json/wp/v2/listings?" . $filter_string;
-    print($url . "\n");
-    $response = wp_remote_get( $url, array (
-     'headers' => $headers,
-    ));
-
-    $wp_listings = json_decode($response['body']);
-    $wp_listing = null;
-
-    print("found:" . count($wp_listings) . " \n");
-    if(count($wp_listings) > 0)
+    if($wp_listing!=null && $wp_listing->acf->auctioned_date == $results_header_json->auctionedDate)
     {
-        $wp_listing = $wp_listings[0];
-
-        if($wp_listing->acf->auctioned_date == $results_header_json->auctionedDate)
-        {
-            print("listing already exists in houseace, skipped! \n");
-            continue;
-        }
+        print("listing already exists in houseace, skipped! \n");
+        continue;
     }
     
     // get listing detail from domain.com.au
     $listing_data = $listings_api->getRequest('https://api.domain.com.au/v1/listings/' . $listing->id);
     $listing_json = json_decode($listing_data);
-    print("\n");
+
     sleep(3);
 
     if(!isset($listing_json->id)){
@@ -181,14 +198,18 @@ foreach($listings_json as $listing)
     $post_data['fields[id]'] = $listing_json->id;
 
     $images_count = 0;
+    $gallery_html = '';
     foreach($listing_json->media as $i => $image)
     {
         if($image->type=='photo')
         {
             $post_data['fields[media][image_'. $images_count .']'] = $image->url;
+            $gallery_html = $gallery_html . "<img src='" . $image->url . "'/>";
             $images_count++;
         }
     }
+
+    $post_data['fields[gallery]'] = $gallery_html;
 
     $post_data['fields[display_price]'] = $listing_json->priceDetails->displayPrice;
     $post_data['fields[sold_details][sold_action]'] = $listing_json->saleDetails->soldDetails->soldAction;
@@ -198,32 +219,50 @@ foreach($listings_json as $listing)
     $post_data['fields[virtual_tour_url]'] = $listing_json->virtualTourUrl;
 
     $post_data['title'] = $listing_json->addressParts->displayAddress;
+    $post_data['content'] = $listing_json->description;
     $post_data['status'] = 'publish';
     
     if($wp_listing!=null)
     {
-        //$url = rest_url( 'wp/v2/listings/' . $wp_listing->id  );
-        $url = "https://houseace.com.au/wp-json/wp/v2/listings/" . $wp_listing->id;
+        $url = $houseace_base_url . '/wp-json/wp/v2/listing/' . $wp_listing->id;
+
     }else{
-        //$url = rest_url( 'wp/v2/listings' );
-        $url = "https://houseace.com.au/wp-json/wp/v2/listings";
+        $url = $houseace_base_url . '/wp-json/wp/v2/listing';
     }
 
-    print('posting to houseace:' . $url . '\n');
-
+    // create or update listing with acf fields
+    print("posting to houseace with acf fields:" . $url . "\n");
     $response = wp_remote_post( $url, array (
      'method'  => 'POST',
+     'timeout' => 45,
      'headers' => $wp_headers,
      'body'    =>  $post_data
     ));
 
-    print('next... \n');
+    var_dump($response);
+    // try to find houseace listing again
+    if($wp_listing==null)
+    {
+        $wp_listing = get_houseace_listing_post($listing->id);
+    }
+
+    // update the listing thumbnails and impress fields
+    $url = $houseace_base_url . '/wp-json/wq/v2/listings/' . $wp_listing->id;
+    print("updating impress plugin fields:" . $url . "\n");
+    $response = wp_remote_post( $url, array (
+     'method'  => 'POST',
+     'timeout' => 45,
+     'headers' => $wp_headers,
+     'body'    =>  $post_data
+    ));
+
+    print("next... \n");
 
 
     $inserted_count++;
-    if($inserted_count == 10)
+    if($inserted_count == 3)
     {
-        // break;
+        break;
     }
 
 }
